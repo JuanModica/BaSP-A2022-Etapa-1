af83ab645d7572dc764c390a6606ffdc7f680cc6
JuanModica <jcmodica18@gmail.com>
2022-09-17 10:51:40 -0300

    BaSP-W05-2022: create semana-05 folder, exercises folder, index-html file and ex01-variables and operators file

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,35 @@
+console.log('--EXERCISE 1: VARIABLES AND OPERATORS');
+
+/* a. Crear dos variables numéricas y utilizar el operador suma para
+ guardar el valor de la suma de ambos numeros en una 3er variable. */
+
+ console.log('\n-Exercise 1.a:');
+ 
+ var num1 = 2;
+ var num2 = 3;
+ var sum = num1 + num2;
+
+ console.log('El resultado: ', sum);
+
+ /* b. Crear dos variables de tipo String y concatenarlas 
+ guardando el resultado en una 3er variable. */
+
+ console.log('\n-Exercise 1.b:');
+
+ var word1 = 'Hello';
+ var word2 = 'World';
+ var text = word1 + ' ' + word2;
+
+ console.log('El resultado: ', text);
+
+ /* c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
+ guardando el resultado de la suma en una 3er variable (utilizar length).*/
+
+ console.log('\n-Exercise 1.c:');
+
+ var word1 = 'Otorrino';
+ var word2 = 'naringologo';
+ var sumWords = word1.length + word2.length;
+
+ console.log('El resultado: ', sumWords);
+
